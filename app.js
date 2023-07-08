2c99edb18ea516835e49a8394ccb9e889c0d85ff
Bhanu <bhanudtg@gmail.com>
2023-07-08 18:17:31 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+// const name = ["Bhanu" , "Pratap" , "Shukla"] // for (let names of name){
+//    console.log(names);
+// }
+
+// console.log(name.map(names => {
+//     return  'Name: ' + names;
+// }));
+// console.log(name);
+// const fetchData = callback => {
+//     const promise = new Promise((resolve, reject)=> {
+//         setTimeout(() => {
+//             resolve('Callback!');
+//         },1500);
+//     });
+//     return promise;
+// };
+
+// setTimeout(() => {
+//     console.log('Timer is done!');
+//     fetchData(text => {
+//         console.log(text);
+//     });
+// },2000);
+
+// console.log('Hello!');
+// console.log('Hi!'); 
+
