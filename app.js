4dee1b99ad7ec2e57e5ac945a8ab8d237300f49e
Bhanu <bhanudtg@gmail.com>
2023-07-08 18:30:33 +0530

    Intial Commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
 
 
-// const name = ["Bhanu" , "Pratap" , "Shukla"] // for (let names of name){
-//    console.log(names);
-// }
+const name = ["Bhanu" , "Pratap" , "Shukla"] // for (let names of name){
+   console.log(names);
+}
 
-// console.log(name.map(names => {
-//     return  'Name: ' + names;
-// }));
-// console.log(name);
-// const fetchData = callback => {
-//     const promise = new Promise((resolve, reject)=> {
-//         setTimeout(() => {
-//             resolve('Callback!');
-//         },1500);
-//     });
-//     return promise;
-// };
+console.log(name.map(names => {
+    return  'Name: ' + names;
+}));
+console.log(name);
+const fetchData = callback => {a
+    const promise = new Promise((resolve, reject)=> {
+        setTimeout(() => {
+            resolve('Callback!');
+        },1500);
+    });
+    return promise;
+};
 
-// setTimeout(() => {
-//     console.log('Timer is done!');
-//     fetchData(text => {
-//         console.log(text);
-//     });
-// },2000);
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData(text => {
+        console.log(text);
+    });
+},2000);
 
-// console.log('Hello!');
-// console.log('Hi!'); 
+console.log('Hello!');
+console.log('Hi!'); 
 
