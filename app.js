142f81f2d05c8a074e4f01320aa24f15f40995a3
Bhanu <bhanudtg@gmail.com>
2023-07-09 18:39:18 +0530

    Sending Response

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 const http = require('http'); 
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Sending Response</title></head>');
+    res.write('<body><h1>Welcome to my Nodejs server!</body>');
+    res.write('</html>');
+    res.end();
 }); 
 
 server.listen(3000);
