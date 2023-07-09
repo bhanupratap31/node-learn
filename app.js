37008c1bab44bf01ac27f3be2606c18e98ce89d4
Bhanu <bhanudtg@gmail.com>
2023-07-09 23:18:19 +0530

    Saving for the day

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-//First node server
-const http = require('http'); 
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="Submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
+
+
+
+
+//Routing Requests
+// const http = require('http'); 
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     if (url === '/'){
+//         res.write('<html>');
+//         res.write('<head><title>Enter Message</title></head>');
+//         res.write('<body><form action="/message" method="POST"><input type="text"><button type="Submit">Send</button></form></body>');
+//         res.write('</html>');
+//         return res.end();
+//     }
     
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Sending Response</title></head>');
-    res.write('<body><h1>Welcome to my Nodejs server!</body>');
-    res.write('</html>');
-    res.end();
-}); 
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>Sending Response</title></head>');
+//     res.write('<body><h1>Welcome to my Nodejs server!</body>');
+//     res.write('</html>');
+//     res.end();
+// }); 
 
-server.listen(3000);
+// server.listen(3000);
 
 /*Basics of javaScript needed for node.js --not complete*/
 
