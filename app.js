b18b7ff2ae36d40aa5024b55a9d5ff79130079cb
Bhanu <bhanudtg@gmail.com>
2023-07-09 18:04:24 +0530

    First Server

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
+//First node server
+const http = require('http'); 
 
+const server = http.createServer((req, res) => {
+    console.log(req);
+}); 
 
+server.listen(3000);
 
-const name = ["Bhanu" , "Pratap" , "Shukla"];  
-   for (let names of name){
-   console.log(names);
-}
+/*Basics of javaScript needed for node.js --not complete*/
 
-console.log(name.map(names => {
-    return  'Name: ' + names;
-}));
-console.log(name);
-const fetchData = callback => {a
-    const promise = new Promise((resolve, reject)=> {
-        setTimeout(() => {
-            resolve('Callback!');
-        },1500);
-    });
-    return promise;
-};
+// const name = ["Bhanu" , "Pratap" , "Shukla"];  
+//    for (let names of name){
+//    console.log(names);
+// }
 
-setTimeout(() => {
-    console.log('Timer is done!');
-    fetchData(text => {
-        console.log(text);
-    });
-},2000);
+// console.log(name.map(names => {
+//     return  'Name: ' + names;
+// }));
+// console.log(name);
+// const fetchData = callback => {a
+//     const promise = new Promise((resolve, reject)=> {
+//         setTimeout(() => {
+//             resolve('Callback!');
+//         },1500);
+//     });
+//     return promise;
+// };
 
-console.log('Hello!');
-console.log('Hi!'); 
+// setTimeout(() => {
+//     console.log('Timer is done!');
+//     fetchData(text => {
+//         console.log(text);
+//     });
+// },2000);
+
+// console.log('Hello!');
+// console.log('Hi!'); 
 
