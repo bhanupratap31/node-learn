827f7c973a703c24352ef719f89ef0768f7127d0
Bhanu <bhanudtg@gmail.com>
2023-07-09 23:30:02 +0530

    Redirecting requests

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+//Redirecting Requests
+const http = require('http'); 
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method; 
+
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="Submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url==='/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302; 
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Sending Response</title></head>');
+    res.write('<body><h1>Welcome to my Nodejs server!</body>');
+    res.write('</html>');
+    res.end();
+}); 
+
+server.listen(3000);
 
 
 
