0242fdf34e0af18012f9e00cdc78ee6634830742
Bhanu <bhanudtg@gmail.com>
2023-07-11 13:49:29 +0530

    2nd

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) => {
         return res.end();
       });
     });
-  };
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
