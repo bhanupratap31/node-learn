a21ad8bb9c4d4fd2c68cdde33f49fe123c502bc8
Bhanu <bhanudtg@gmail.com>
2023-07-18 19:59:04 +0530

    Adding html to respective routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product',(req, res, next) => {
-    // console.log('Fist middleware');
-    res.send('<form action= "/admin/add-product" method ="POST"><input type= "text" name= "title"><button type ="submit">Add Product</button></input></form>');
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
-router.post('/product', (req, res, next) => { //triggers only for POST request
-    console.log(req.body); //will be parsed by the body parser
-    res.redirect('/'); //to redirect to / route
-})
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
