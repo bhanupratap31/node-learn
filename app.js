eb7030deb65e798137703e8bfd9fbcc14daebf1e
Bhanu <bhanudtg@gmail.com>
2023-07-19 23:07:43 +0530

    Serving files statically

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -18,6 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000);
+
 
 
 
